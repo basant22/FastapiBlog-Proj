dc5c7884c0a2e4bb195b90ff44e40a680f9c0264
basant22 <basant.kumar22@gmail.com>
2026-08-13 16:51:04 +0530

    fix test case

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,4 +6,4 @@ client = TestClient(app)
 def test_read_root():
     response = client.get("/")
     # Checks if the server responds (adjust expected status code as needed)
-    assert response.status_code in [200, 404]
+    assert response.status_code in [200,302, 404]
